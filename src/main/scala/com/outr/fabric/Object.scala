1e15992c464531bb5e005c527fb5f94ff6dc8b99
Matt Hicks <matt@outr.com>
2016-06-23 16:54:01 -0500

    Minor updates

--- src/main/scala/com/outr/fabric/Object.scala
+++ src/main/scala/com/outr/fabric/Object.scala
@@ -1,5 +1,17 @@
 package com.outr.fabric
 
-class Object {
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
 
+@js.native
+@JSName("fabric.Object")
+class Object extends js.Object {
+  var angle: Double = js.native
+  var backgroundColor: String = js.native
+  var borderColor: String = js.native
+  var borderDashArray: Array[Double] = js.native
+  var borderOpacityWhenMoving: Double = js.native
+  var borderScaleFactor: Double = js.native
+  var centeredRotation: Boolean = js.native
+  var centeredScaling: Boolean = js.native
 }
---
+++ src/main/scala/com/outr/fabric/StaticCanvas.scala
@@ -0,0 +1,13 @@
+package com.outr.fabric
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
+@js.native
+@JSName("fabric.StaticCanvas")
+class StaticCanvas(el: String) extends js.Object {
+  var allowTouchScrolling: Boolean = js.native
+  var backgroundColor: String = js.native
+  var backgroundImage: Image = js.native
+
+}
